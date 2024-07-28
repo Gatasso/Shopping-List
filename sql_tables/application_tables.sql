118eeb4807dcd2f2c90c2ce6345b95896913ec7c
Gatasso <galassoworkstation@gmail.com>
2024-07-27 21:16:29 -0300

    :bulb: Build: :bulb: Creating the project w/ Spring Initializr

---
+++ sql_tables/application_tables.sql
@@ -0,0 +1,68 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema db_listaMercado
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema db_listaMercado
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `db_listaMercado` ;
+USE `db_listaMercado` ;
+
+-- -----------------------------------------------------
+-- Table `db_listaMercado`.`tb_product`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_listaMercado`.`tb_product` (
+  `id_product` INT NOT NULL,
+  `name_product` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id_product`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_listaMercado`.`tb_list`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_listaMercado`.`tb_list` (
+  `id_list` INT NOT NULL AUTO_INCREMENT,
+  `creation_date` DATE NOT NULL,
+  `shop_name` VARCHAR(50) NULL,
+  `total` DOUBLE NULL,
+  `status` INT NOT NULL,
+  PRIMARY KEY (`id_list`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_listaMercado`.`tb_list_item`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_listaMercado`.`tb_list_item` (
+  `id_item` INT NOT NULL AUTO_INCREMENT,
+  `quantity` DOUBLE NULL,
+  `total_price` DOUBLE NULL,
+  `shop_status` INT NULL,
+  `tb_product_id_product` INT NOT NULL,
+  `tb_list_id_list` INT NOT NULL,
+  PRIMARY KEY (`id_item`),
+  INDEX `fk_tb_list_item_tb_product_idx` (`tb_product_id_product` ASC) VISIBLE,
+  INDEX `fk_tb_list_item_tb_list1_idx` (`tb_list_id_list` ASC) VISIBLE,
+  CONSTRAINT `fk_tb_list_item_tb_product`
+    FOREIGN KEY (`tb_product_id_product`)
+    REFERENCES `db_listaMercado`.`tb_product` (`id_product`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_tb_list_item_tb_list1`
+    FOREIGN KEY (`tb_list_id_list`)
+    REFERENCES `db_listaMercado`.`tb_list` (`id_list`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ src/test/java/dev/galasso/marketApp/MarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.galasso.marketApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
