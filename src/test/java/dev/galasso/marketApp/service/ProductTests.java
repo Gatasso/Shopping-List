6035160fb35efe66b41fde52751bcc901c5f8ddf
Gatasso <galassoworkstation@gmail.com>
2024-07-29 16:06:16 -0300

    :sparkles: Feat: Implement Item and ShopList Infraestucture. Updates the tests to new features :sparkles:

--- src/test/java/dev/galasso/marketApp/service/ProductTests.java
+++ src/test/java/dev/galasso/marketApp/service/ProductTests.java
@@ -24,68 +24,68 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 public class ProductTests {
 
-   @Mock
-   private ProductRepository repository;
+    @Mock
+    private ProductRepository repository;
 
-   @InjectMocks
-   private ProductServiceImpl service;
+    @InjectMocks
+    private ProductServiceImpl service;
 
-   private Integer existingId = 1;
-   private Integer nonExistingId = 100;
-   private String keyWord = "Pickles";
-   private Product newProduct;
-   private Product createdProduct;
-   private ArrayList<Product> productsList;
+    private Integer existingId = 1;
+    private Integer nonExistingId = 100;
+    private String keyWord = "Pickles";
+    private Product newProduct;
+    private Product createdProduct;
+    private ArrayList<Product> productsList;
 
-   @BeforeEach
-   public void setup() throws Exception{
-      newProduct = new Product();
-      newProduct.setName("Pickles");
+    @BeforeEach
+    public void setup() throws Exception {
+        newProduct = new Product();
+        newProduct.setName("Pickles");
 
-      createdProduct = new Product();
-      createdProduct.setName("Pickles");
-      createdProduct.setId(1);
+        createdProduct = new Product();
+        createdProduct.setName("Pickles");
+        createdProduct.setId(1);
 
-      productsList = new ArrayList<Product>();
-      Product p1,p2;
-      p1 = new Product();
-      p1.setId(2);
-      p1.setName("Pickles argentino");
-      p2 = new Product();
-      p2.setId(3);
-      p2.setName("Pickles Tailandês");
-      productsList.add(p1);
-      productsList.add(p2);
+        productsList = new ArrayList<Product>();
+        Product p1, p2;
+        p1 = new Product();
+        p1.setId(2);
+        p1.setName("Pickles argentino");
+        p2 = new Product();
+        p2.setId(3);
+        p2.setName("Pickles Tailandês");
+        productsList.add(p1);
+        productsList.add(p2);
 
-      Mockito.when(repository.save(newProduct)).thenReturn(createdProduct);
-      Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(new Product()));
-      Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.ofNullable(null));
-      Mockito.when(repository.findAllByNameContaining("Pepino")).thenReturn(new ArrayList<Product>());
-      Mockito.when(repository.findAllByNameContaining(keyWord)).thenReturn(productsList);
-   }
+        Mockito.when(repository.save(newProduct)).thenReturn(createdProduct);
+        Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(new Product()));
+        Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.ofNullable(null));
+        Mockito.when(repository.findAllByNameContaining("Pepino")).thenReturn(new ArrayList<Product>());
+        Mockito.when(repository.findAllByNameContaining(keyWord)).thenReturn(productsList);
+    }
 
-   @Test
-   public void shouldRegisterProduct(){
-      assertEquals(service.createProduct(newProduct), createdProduct);
-   }
+    @Test
+    public void shouldRegisterProduct() {
+        assertEquals(service.createProduct(newProduct), createdProduct);
+    }
 
-   @Test
-   public void shouldReturnById(){
-      assertNotNull(service.findById(existingId));
-   }
+    @Test
+    public void shouldReturnById() {
+        assertNotNull(service.findById(existingId));
+    }
 
-   @Test
-   public void shouldNotReturnById(){
-      assertNull(service.findById(nonExistingId));
-   }
+    @Test
+    public void shouldNotReturnById() {
+        assertNull(service.findById(nonExistingId));
+    }
 
-   @Test
-   public void shouldReturnListByKeyWord(){
-      assertFalse(service.getProductByKeyWord(keyWord).isEmpty());
-   }
+    @Test
+    public void shouldReturnListByKeyWord() {
+        assertFalse(service.getProductByKeyWord(keyWord).isEmpty());
+    }
 
-   @Test
-   public void shouldReturnEmptyList(){
-       assertTrue(service.getProductByKeyWord("B").isEmpty());
-   }
+    @Test
+    public void shouldReturnEmptyList() {
+        assertTrue(service.getProductByKeyWord("B").isEmpty());
+    }
 }
